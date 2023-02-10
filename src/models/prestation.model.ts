8706dbd0a4c906ea844ef96aae56d5a7d7d8891d
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-02-10 12:21:11 +0100

    feat: Ajout du model de prestation :sparkles:

---
+++ src/models/prestation.model.ts
@@ -0,0 +1,33 @@
+class PrestationModel {
+  id!: number;
+  idEntreprise: number;
+  titre: string;
+  categories: string[];
+  tauxHoraire: number;
+  localisation: string;
+  description: string;
+  imageURL: string;
+  datePublication: string = new Date().toLocaleDateString(); //Date fixée à la création
+  nombreRealisations: number = 0;
+  note: number = 0;
+
+  constructor(
+    idEntreprise: number,
+    titre: string,
+    tauxHoraire: number,
+    description: string = "",
+    imageURL: string,
+    localisation: string,
+    categories: string[]
+  ) {
+    this.idEntreprise = idEntreprise;
+    this.titre = titre;
+    this.tauxHoraire = tauxHoraire;
+    this.description = description;
+    this.imageURL = imageURL;
+    this.localisation = localisation;
+    this.categories = categories;
+  }
+}
+
+export default PrestationModel;
