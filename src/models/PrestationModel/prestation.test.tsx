d5d81523a66d9a5f86116afa98480dc1e01029aa
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-02-10 13:00:17 +0100

    test: Ajout test de création PrestationModel :white_check_mark:

---
+++ src/models/PrestationModel/prestation.test.tsx
@@ -0,0 +1,37 @@
+import PrestationModel from "./prestation.model";
+
+describe("Création d'une prestation", () => {
+  it("doit avoir des valeurs par défaut", () => {
+    const prestation = new PrestationModel(
+      1,
+      "title",
+      10,
+      "",
+      "image.jpg",
+      "location",
+      ["category"]
+    );
+    expect(prestation.datePublication).toEqual(new Date().toLocaleDateString());
+    expect(prestation.nombreRealisations).toEqual(0);
+    expect(prestation.note).toEqual(0);
+  });
+
+  it("doit avoir des valeurs correspondantes à celles renseignées", () => {
+    const prestation = new PrestationModel(
+      1,
+      "titre",
+      10,
+      "description",
+      "image.jpg",
+      "localisation",
+      ["categorie1"]
+    );
+    expect(prestation.idEntreprise).toEqual(1);
+    expect(prestation.titre).toEqual("titre");
+    expect(prestation.tauxHoraire).toEqual(10);
+    expect(prestation.description).toEqual("description");
+    expect(prestation.imageURL).toEqual("image.jpg");
+    expect(prestation.localisation).toEqual("localisation");
+    expect(prestation.categories).toEqual(["categorie1"]);
+  });
+});
