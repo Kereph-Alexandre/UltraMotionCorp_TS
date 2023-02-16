05e1ccf8bc3a887db2fd2b3ec16f46b551b797e9
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-02-16 11:31:32 +0100

    fix(Accueil): modifie les Cartes & ajoute un rendu conditonnel :bug:

--- src/composants/Carte/Carte.tsx
+++ src/composants/Carte/Carte.tsx
@@ -1,7 +1,6 @@
 import { CardContent, Chip, Divider, Rating } from "@mui/material";
 import Card from "@mui/material/Card";
 
-import CardActionArea from "@mui/material/CardActionArea";
 import CardMedia from "@mui/material/CardMedia";
 
 import "../../styles/Carte.scss";
@@ -16,23 +15,21 @@ import PrestationModel from "../../models/PrestationModel/prestation.model"; //I
 const Carte: React.FC<PrestationModel> = (props) => {
   return (
     <Card className="cartePrestation">
-      <CardActionArea>
-        <CardMedia component="img" image={props.imageURL} />
-        <CardContent className="contenuCarte">
-          <div className="enteteCarte">
-            <h2 className="elementTitre titreCarte">{props.titre}</h2>
-            <p className="elementTexte prixCarte">{props.tauxHoraire}€/H</p>
-          </div>
+      <CardMedia component="img" image={props.imageURL} />
+      <CardContent className="contenuCarte">
+        <div className="enteteCarte">
+          <h2 className="elementTitre titreCarte">{props.titre}</h2>
+          <p className="elementTexte prixCarte">{props.tauxHoraire}€/H</p>
           <Divider />
-          <p className="elementTexte texteCarte">{props.description}</p>
-          <div className="piedDePageCarte">
-            {props.categories.map((categorie) => (
-              <Chip label={categorie} />
-            ))}
-            <Rating value={props.note} readOnly />
-          </div>
-        </CardContent>
-      </CardActionArea>
+        </div>
+        <p className="elementTexte texteCarte">{props.description}</p>
+        <div className="piedDePageCarte">
+          {props.categories.map((categorie) => (
+            <Chip label={categorie} />
+          ))}
+          <Rating value={props.note} readOnly />
+        </div>
+      </CardContent>
     </Card>
   );
 };
--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Banniere from "../composants/Banniere/Banniere";
-import Carte from "../composants/Carte/Carte";
+import GrilleCartes from "../composants/Presentation/GrilleCartes/GrilleCartes";
 import PrestationModel from "../models/PrestationModel/prestation.model";
 import PrestationService from "../service/prestation.service";
 
@@ -10,10 +10,12 @@ const Accueil = () => {
   );
 
   useEffect(() => {
-    PrestationService.obtenirPrestations().then((prestations) =>
-      setListePrestations(prestations)
-    );
+    PrestationService.obtenirPrestations().then((prestations) => {
+      setListePrestations(prestations);
+    });
   }, []);
+
+  useEffect(() => console.log(listePrestations), [listePrestations]);
   return (
     <>
       <Banniere
@@ -21,9 +23,12 @@ const Accueil = () => {
         texte="truc"
         imageURL="https://img.freepik.com/free-photo/handyman-construction-site-process-drilling-wall-with-perforator_169016-12114.jpg?w=1800&t=st=1676042624~exp=1676043224~hmac=62e6514264dca8a3d4f75a45d6416e64e13505af42ebbb719575ced84e746053"
       />
-      {listePrestations.map((prestation) => (
-        <Carte {...prestation} />
-      ))}
+
+      {listePrestations.length > 0 ? (
+        <GrilleCartes listePrestations={listePrestations} />
+      ) : (
+        <p>...Loading</p>
+      )}
     </>
   );
 };
