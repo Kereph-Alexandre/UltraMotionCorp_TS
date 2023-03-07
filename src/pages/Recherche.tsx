f20f5b93c62bd08cac9983445b858379070ef916
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-03-07 16:45:11 +0100

    feat(Recherche) : Ajoute la possibilité de rechercher dans la description et les catégories d'une prestation :sparkles:

--- src/pages/Recherche.tsx
+++ src/pages/Recherche.tsx
@@ -5,17 +5,43 @@ import GrilleCartes from "../composants/Presentation/GrilleCartes/GrilleCartes";
 import PrestationModel from "../models/PrestationModel/prestation.model";
 import PrestationService from "../service/prestation.service";
 
+/**
+ * Page de Recherche de prestation
+ * @returns La page des résultats d'une recherche
+ */
 const Recherche: React.FC = () => {
   const [resultats, setResultats] = useState<PrestationModel[]>([]);
 
   const location = useLocation();
   const rechercheParams = new URLSearchParams(location.search);
-  let motRecherche = rechercheParams.get("motRecherche") ?? "";
+  const motRecherche = rechercheParams.get("motRecherche") ?? "";
 
   useEffect(() => {
+    const rechercheNorm = motRecherche
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
     PrestationService.obtenirPrestations()
       .then((data) =>
-        data.filter((prestation) => prestation.titre.includes(motRecherche))
+        data.filter(
+          (prestation) =>
+            prestation.titre
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+              .toLowerCase()
+              .includes(rechercheNorm.toLowerCase()) ||
+            prestation.description
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+              .toLowerCase()
+              .includes(rechercheNorm.toLowerCase()) ||
+            prestation.categories.find((categorie) =>
+              categorie
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .toLowerCase()
+                .includes(rechercheNorm.toLowerCase())
+            )
+        )
       )
       .then((data) => setResultats(data));
   }, [motRecherche]);
